6d8c97730f24ba9a4fe60556cc2d736b247ead23
Majkell99 <163130971+Majkell99@users.noreply.github.com>
2026-03-06 11:04:15 +0100

    Add files via upload

--- C++/main.c
+++ C++/main.c
@@ -1,28 +1,12 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
-#include <limits.h>
 
-int main(void) {
-    double d = 1;
-    float f = 1;
+int main() {
+    float a = 3.3;
+    float b = 10.7;
+    float c = 20.5;
 
-    char c = 'A';
-    bool b = false;
+    float suma = a+b+c;
+    float srednia = suma/3;
 
-    f += 1.0f;
-
-    unsigned int ui = INT_MAX + 1U;
-
-    printf("l = %ld\n", 3L);
-
-    printf("c = %c\n", c);
-    printf("b = %d\n", b);
-
-    // Poniższe instrukcje zostały dodane po to, aby kompilator nie zgłaszał
-    // błędów związanych z nieużywanymi zmiennymi.
-    ui += 1;
-    d += 0.0;
-
-    return EXIT_SUCCESS;
+    return 0;
 }
