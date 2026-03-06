7b590cae1c58f0b4a0003bf976cdad293c80ae7b
Majkell99 <163130971+Majkell99@users.noreply.github.com>
2026-03-06 11:07:11 +0100

    Add files via upload

--- C++/main.c
+++ C++/main.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+int main() {
 
-    unsigned char c = 50;
-    printf("Ascii z 50 to: %c", c);
+    int* tab = (int*)malloc(10*sizeof(int));
+
+    if (tab == NULL)
+    {
+        return 1;
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+        tab[i] = rand() % 100;
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf("%d ", tab[i]);
+    }
+
+    printf("\n");
+
+    int* newtab = (int*)realloc(tab, 20*sizeof(int));
+
+    if (newtab == NULL)
+    {
+        return 1;
+    }
+
+    for(int i = 10; i < 20; i++)
+    {
+        newtab[i] = 0;
+    }
+
+    for(int i = 0; i < 20; i++)
+    {
+        printf("%d ", newtab[i]);
+    }
 
     return 0;
 }
