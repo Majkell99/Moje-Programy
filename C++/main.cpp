4686d99fc0b189820a8e0fa22a44a8204cb163a6
Majkell99 <163130971+Majkell99@users.noreply.github.com>
2026-03-06 11:42:20 +0100

    Add files via upload

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,6 +1,19 @@
+#include "matlab.hpp"
 #include <iostream>
+// Wersje bibliotek standardowych znanych z języka C, ale zaimplementowanych
+// dla C++, mają przedrostek "c" (np. stdlib.h -> cstdlib).
+#include <cstdlib>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    //int v[] = {1, 2, 3};
+    //print_vector(v, 3);
+
+//    MatVect v_(5);
+//    std::cout << v_.size
+//    for(int i = 0 ; i < 5; i++)
+//    {
+//        Mat
+//    }
+
+    return EXIT_SUCCESS;
 }
