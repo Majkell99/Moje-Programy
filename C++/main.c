3e70922646a340c20e543ff3d879b10568601d81
Majkell99 <163130971+Majkell99@users.noreply.github.com>
2026-03-06 11:02:39 +0100

    Add files via upload

--- C++/main.c
+++ C++/main.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
-int main() {
-    int n = 7;
-
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= i; j++) {
-            printf("$");
-        }
-        printf ("\n");
+int pierwsza(int n, int dzielnik)
+{
+    if(n % dzielnik == 0)
+    {
+        return 0;
     }
+
+    return pierwsza(n, dzielnik + 1);
+}
+
+int main()
+{
+    if(pierwsza(6) == 0)
+    {
+        printf("Nie pierwsza");
+    }
+    else
+    {
+        printf("Pierwsza");
+    }
+
     return 0;
 }
+// NIE WIEM
